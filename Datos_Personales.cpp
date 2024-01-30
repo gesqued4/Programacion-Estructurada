02750443983e452bfcbb617ea13806c4e633d993
ryzex404 <gerardo.esqueda9479@alumnos.udg.mx>
2024-01-30 11:42:20 -0600

    first commit

---
+++ Datos_Personales.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+using namespace std;
+    int main () {
+        /*************************Presentacion*************************
+        NOMBRE: Gerardo Esqueda Padilla
+        FECHA: 26 de Enero de 2024
+        PROGRAMA: Datos_Personales.cpp
+        CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+        INGENIERIA EN COMPUTACION / 2DO SEMESTRE
+        PROFESOR: Sergio Franco Casillas
+        DESCRIPCION: Programa para imprimir mis datos personales
+        **************************************************************/
+       printf("Nombre: Gerardo Esqueda Padilla\nEdad: 19\nLugar de residencia: Tepatitlan de Morelos\n"
+       "Fecha: 29/01/24\nCarrera: Ing en Computacion\nSemestre: Segundo Semestre");
+
+    return 0;
+
+    }
---
+++ Hola_Mundo_VS.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+using namespace std;
+	int main() {
+		printf("Hola Mundo!!");
+
+		return 0;
+
+	}
