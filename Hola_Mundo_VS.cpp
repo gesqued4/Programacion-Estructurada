2a0028da15f62ab0599b5650f74537e1e8fcdaca
ryzex404 <gerardo.esqueda9479@alumnos.udg.mx>
2024-01-30 20:33:08 -0600

    Cambio terminal en Visual

--- Hola_Mundo_VS.cpp
+++ Hola_Mundo_VS.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 using namespace std;
 	int main() {
-		printf("Hola Mundo!!\nCambio para probar git");
+		printf("Hola Mundo!!\nCambio para probar git\nSegundo cambio para probar la terminal en Visual Studio");
 
 		return 0;
 
