5f387cccbf1d1719f28612aa8196c740a838fc2a
ryzex404 <gerardo.esqueda9479@alumnos.udg.mx>
2024-02-03 22:04:11 -0600

    Actividad de Programacion 2

--- Hola_Mundo.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-using namespace std;
-	int main() {
-		printf("Hola Mundo!!\nCambio para probar git\nSegundo cambio para probar la terminal en Visual Studio");
-
-		return 0;
-
-	}
